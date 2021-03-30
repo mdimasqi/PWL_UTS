ba008a50fe0e8c8cc0f1af977aeee30ce4893b62
= <=>
2021-03-30 12:05:49 +0700

    Routes web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('barang', BarangController::class);
+Route::get('/',[BarangController::class, 'index']);
