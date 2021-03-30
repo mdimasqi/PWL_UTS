bc542bc2eee52e1e5a59fe444cf574a4a22b399c
= <=>
2021-03-30 11:20:02 +0700

    Membuat View Create

---
+++ resources/views/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layout')
+ 
+@section('content')
+ 
+<div class="container mt-5">
+ 
+ <div class="row justify-content-center align-items-center">
+    <div class="card" style="width: 24rem;">
+    <div class="card-header">
+        Tambah Barang
+    </div>
+    <div class="card-body">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+            </div>
+        @endif
+    <form method="post" action="{{ route('barang.store') }}" id="myForm">
+        @csrf
+        <div class="form-group">
+            <label for="id_barang">ID_Barang</label> 
+                <input type="text" name="id_barang" class="form-control" id="id_barang" aria-describedby="id_barang" > 
+        </div>
+        <div class="form-group">
+            <label for="kode_barang">Kode Barang</label> 
+                <input type="text" name="kode_barang" class="form-control" id="kode_barang" aria-describedby="kode_barang" > 
+        </div>
+        <div class="form-group">
+            <label for="nama_barang">Nama Barang</label>  
+            <input type="text" name="nama_barang" class="form-control" id="nama_barang" aria-describedby="nama_barang" > 
+        </div>
+        <div class="form-group">
+            <label for="kategori_barang">Kategori Barang</label> 
+            <input type="text" name="kategori_barang" class="form-control" id="kategori_barang" aria-describedby="kategori_barang" > 
+        </div>
+        <div class="form-group">
+            <label for="harga">Harga</label> 
+                <input type="Harga" name="harga" class="form-control" id="harga" aria-describedby="harga" > 
+        </div>
+        <div class="form-group">
+            <label for="qty">QTY</label> 
+                <input type="qty" name="qty" class="form-control" id="qty" aria-describedby="qty" > 
+        </div>
+        <div class="form-group">
+        <button type="submit" class="btn btn-primary">Submit</button>
+ </form>
+ </div>
+ </div>
+ </div>
+ </div>
+@endsection
